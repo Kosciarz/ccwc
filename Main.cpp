60011b2e34dd8cddd1c99229cb36f71e92be0e30
Kosciarz <bartoszbachul1@gmail.com>
2025-01-23 17:22:21 +0100

    added 3 new functions to the tool; updated README.md; updated .gitignore

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,79 @@
+#include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <vector>
 
-int main()
+std::vector<std::string> ConvertArgvToString(int argc, char** argv)
 {
-    std::cout << "works!\n";
+    std::vector<std::string> args{};
+    for (int i = 0; i < argc; ++i)
+    {
+        args.emplace_back(argv[i]);
+    }
+    return args;
+}
+
+void CountBytes(std::filesystem::path filePath)
+{
+    std::ifstream file(filePath);
+
+    char ch;
+    int numberOfBytes{};
+    while (file.get(ch))
+    {
+        numberOfBytes++;
+    }
+    std::cout << numberOfBytes << '\n';
+
+    file.close();
+}
+
+void CountLines(std::filesystem::path filePath)
+{
+    std::ifstream file(filePath);
+
+    int numberOfLines{};
+    std::string fileLine{};
+    while (std::getline(file, fileLine))
+    {
+        numberOfLines++;
+    }
+    std::cout << numberOfLines << '\n';
+
+    file.close();
+}
+
+void CountWords(std::filesystem::path filePath)
+{
+    std::ifstream file(filePath);
+
+    int numberOfWords{};
+    std::string fileLine{};
+    while (std::getline(file, fileLine))
+    {
+        std::stringstream sstream(fileLine);
+        std::string word{};
+        while(sstream >> word)
+        {
+            numberOfWords++;
+        }
+    }
+    std::cout << numberOfWords << '\n';
+
+    file.close();
+}
+
+int main(int argc, char** argv)
+{
+    std::vector<std::string> args = ConvertArgvToString(argc, argv);
+
+    std::string operation          = args[1];
+    std::filesystem::path filePath = args[2];
+
+    if (operation == "-c")
+        CountBytes(filePath);
+    if (operation == "-l")
+        CountLines(filePath);
+    if (operation == "-w")
+        CountWords(filePath);
 }
---
+++ README.md
@@ -0,0 +1,8 @@
+# CCWC
+
+This is a C++ implementation of the wc Linux tool written by me.
+
+It supports four different options:
+
+1. -c: Count the number of bytes in the input file.
+2. -l: Count the number of lines in the input file.
