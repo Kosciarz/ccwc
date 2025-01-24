bd21fd10388915e912caa573ddd1657531ea8c32
Kosciarz <bartoszbachul1@gmail.com>
2025-01-24 15:03:03 +0100

    add new functionality; update the readme

--- Main.cpp
+++ Main.cpp
@@ -4,67 +4,49 @@
 #include <regex>
 #include <vector>
 
-size_t CountBytes(const std::filesystem::path& filePath)
-{
-    auto byteCount = std::filesystem::file_size(filePath);
-    return byteCount;
-}
+size_t CountBytes(const std::filesystem::path& filePath) { return std::filesystem::file_size(filePath); }
 
-size_t CountLines(const std::filesystem::path& filePath)
+size_t CountLines(std::wifstream& file)
 {
-    std::ifstream file(filePath);
-    if (!file.is_open())
-        throw std::runtime_error("Cannot open a file with path: " / filePath);
-
     size_t lineCount{};
-    std::string fileLine{};
+    std::wstring fileLine{};
+    file.clear();
+    file.seekg(0, std::ios::beg);
     while (std::getline(file, fileLine))
     {
         lineCount++;
     }
-
-    file.close();
     return lineCount;
 }
 
-size_t CountWords(const std::filesystem::path& filePath)
+size_t CountWords(std::wifstream& file)
 {
-    std::ifstream file(filePath);
-    if (!file.is_open())
-        throw std::runtime_error("Cannot open a file with path: " / filePath);
-
     size_t wordCount{};
-    std::string fileLine{};
+    std::wstring fileLine{};
+    file.clear();
+    file.seekg(0, std::ios::beg);
     while (std::getline(file, fileLine))
     {
-        std::stringstream sstream(fileLine);
-        std::string word{};
+        std::wstringstream sstream(fileLine);
+        std::wstring word{};
         while (sstream >> word)
         {
             wordCount++;
         }
     }
-
-    file.close();
     return wordCount;
 }
 
-size_t CountCharacters(const std::filesystem::path& filePath)
+size_t CountCharacters(std::wifstream& file)
 {
-    std::wifstream file(filePath);
-    file.imbue(std::locale(std::locale(), new std::codecvt_utf8<wchar_t>));
-
-    if (!file.is_open())
-        throw std::runtime_error("Cannot open a file with path: " / filePath);
-
     size_t characterCount{};
     wchar_t ch{};
+    file.clear();
+    file.seekg(0, std::ios::beg);
     while (file.get(ch))
     {
         characterCount++;
     }
-
-    file.close();
     return characterCount;
 }
 
@@ -72,49 +54,46 @@ void DisplayHelp()
 {
     std::cout << "Usage: ccwc [OPTION]... [FILE]..." << '\n'
               << '\n'
-              << "The options are as below:" << '\n'
+              << "The options are:" << '\n'
               << "  -c\tprint the byte counts" << '\n'
               << "  -m\tprint the character counts" << '\n'
               << "  -l\tprint the newline counts" << '\n'
               << "  -w\tprint the word counts" << '\n'
-              << "      --help\tdisplay this help and exit" << '\n';
+              << "  --help\tdisplay this help and exit" << '\n';
 }
 
-void PerformOperation(const std::filesystem::path& filePath, char op)
-{
-    switch (op)
-    {
-        case 'c':
-            std::cout << CountBytes(filePath) << '\n';
-            break;
-        case 'l':
-            std::cout << CountLines(filePath) << '\n';
-            break;
-        case 'w':
-            std::cout << CountWords(filePath) << '\n';
-            break;
-        case 'm':
-            std::cout << CountBytes(filePath) << '\n';
-            break;
-        default:
-            std::cerr << "Invalid option. Use -c, -l, -w or -m." << '\n';
-    }
-}
-
-bool IsValidOption(const std::string& op)
-{
-    return (op.length() == 2 && op[0] == '-') ? true : false;
-}
+bool IsValidOption(const std::string& op) { return (op.length() == 2 && op[0] == '-') ? true : false; }
 
 int main(int argc, char* argv[])
 {
-    if (std::string(argv[1])== "--help")
+    if (argc < 2)
+    {
+        DisplayHelp();
+        return 1;
+    }
+
+    if (std::string(argv[1]) == "--help")
     {
         DisplayHelp();
         return 0;
     }
 
-    if (argc == 2 && std::cin.peek())
+    if (argc == 2 && std::filesystem::exists(argv[1]))
+    {
+        std::filesystem::path inputFilePath = argv[1];
+        std::wifstream inputFile(inputFilePath);
+        inputFile.imbue(std::locale(inputFile.getloc(), new std::codecvt_utf8<wchar_t>));
+
+        if (!inputFile.is_open())
+            std::cerr << "Could not open file " << inputFilePath.string() << '\n';
+
+        size_t lineCount = CountLines(inputFile);
+        size_t wordCount = CountWords(inputFile);
+
+        std::cout << lineCount << "  " << wordCount << "  " << CountBytes(inputFilePath) << '\n';
+        inputFile.close();
+    }
+    else if (argc == 2 && std::cin.peek() != std::istream::traits_type::eof())
     {
         if (!IsValidOption(argv[1]))
         {
@@ -122,56 +101,81 @@ int main(int argc, char* argv[])
             return 1;
         }
 
-        std::filesystem::path outputPath = "outputFile.txt";
-        std::ofstream outputFile(outputPath);
+        std::filesystem::path outputFilePath = "outputFile.txt";
+        std::ofstream outputFile(outputFilePath);
         if (!outputFile.is_open())
-            throw std::runtime_error("Could not open file " / outputPath);
+            std::cerr << "Could not open file " << outputFilePath.string() << '\n';
 
-        std::string line;            
+        std::string line;
         while (std::getline(std::cin, line))
         {
             outputFile << line << '\n';
         }
+        outputFile.close();
 
-        outputFile.close();        
+        std::wifstream inputFile(outputFilePath);
+        inputFile.imbue(std::locale(inputFile.getloc(), new std::codecvt_utf8<wchar_t>));
+        if (!inputFile.is_open())
+            std::cerr << "Could not open file " << outputFilePath.string() << '\n';
 
-        PerformOperation(outputPath, argv[1][1]);
-
-        std::filesystem::remove(outputPath);
-    }
-    else if (argc == 2)
-    {
-        std::filesystem::path filePath = argv[1];
-        try
+        std::string option = argv[1];
+        switch (option.at(1))
         {
-            size_t lineCount = CountLines(filePath);
-            size_t wordCount = CountWords(filePath);
-            size_t byteCount = CountBytes(filePath);
-            std::cout << lineCount << '\t' << wordCount << '\t' << byteCount << '\n';
+        case 'c':
+            std::cout << CountBytes(outputFilePath) << '\n';
+            break;
+        case 'l':
+            std::cout << CountLines(inputFile) << '\n';
+            break;
+        case 'w':
+            std::cout << CountWords(inputFile) << '\n';
+            break;
+        case 'm':
+            std::cout << CountCharacters(inputFile) << '\n';
+            break;
+        default:
+            std::cerr << "Invalid option. Use -c, -l, -w or -m." << '\n';
         }
-        catch (const std::exception& e)
-        {
-            std::cout << "Error: " << e.what() << '\n';
-        }
+        std::filesystem::remove(outputFilePath);
     }
     else if (argc == 3)
-    {   
+    {
         if (!IsValidOption(argv[1]))
         {
             DisplayHelp();
             return 1;
         }
 
-        try
-        {
-            PerformOperation(argv[2], argv[1][1]);
-        }
-        catch (const std::exception& e)
-        {
-            std::cout << "Error: " << e.what() << '\n';
-        }
-        return 0;
-    }
+        std::filesystem::path inputFilePath = argv[2];
+        std::wifstream inputFile(inputFilePath);
+        inputFile.imbue(std::locale(inputFile.getloc(), new std::codecvt_utf8<wchar_t>));
 
-    
+        if (!inputFile.is_open())
+            std::cerr << "Could not open file " << inputFilePath.string() << '\n';
+
+        std::string option = argv[1];
+        switch (option.at(1))
+        {
+        case 'c':
+            std::cout << CountBytes(inputFilePath) << '\n';
+            break;
+        case 'l':
+            std::cout << CountLines(inputFile) << '\n';
+            break;
+        case 'w':
+            std::cout << CountWords(inputFile) << '\n';
+            break;
+        case 'm':
+            std::cout << CountCharacters(inputFile) << '\n';
+            break;
+        default:
+            std::cerr << "Invalid option. Use -c, -l, -w or -m." << '\n';
+        }
+        inputFile.close();
+    }
+    else
+    {
+        DisplayHelp();
+        return 1;
+    }
 }
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # CCWC
 
-This is a C++ implementation of the wc Linux tool written by me.
+This is a C++ implementation of the `wc` Linux tool written by me.
 
 It supports four different options:
 
-1. -c: Count the number of bytes in the input file.
-2. -l: Count the number of lines in the input file.
-3. -w: Count the number of words in the input file.
-4. -m: Count the number of characters in the input file.
+1. `-c`: Count the number of bytes in the input file.
+2. `-l`: Count the number of lines in the input file.
+3. `-w`: Count the number of words in the input file.
+4. `-m`: Count the number of characters in the input file.
 
 ## Installation
 
@@ -15,8 +15,42 @@ To use CCWC:
 
 1. Clone the git repository:
 
-     `git clone https://github.com/Kosciarz/ccwc.git`
+    ```sh
+    git clone https://github.com/Kosciarz/ccwc.git
+    ```
 
 2. Build the executable:
 
-    'make'
+    ```sh
+    make
+    ```
+
+## Usage
+
+You can use CCWC with a file or standard input.
+
+### With a file
+
+-Linux
+    ```sh
+    ./ccwc -c filename.txt  # Count bytes
+    ./ccwc -l filename.txt  # Count lines
+    ./ccwc -w filename.txt  # Count words
+    ./ccwc -m filename.txt  # Count characters
+    ```
+
+-Windows
+    ```bash
+    .\ccwc.exe -c filename.txt  # Count bytes
+    .\ccwc.exe -l filename.txt  # Count lines
+    .\ccwc.exe -w filename.txt  # Count words
+    .\ccwc.exe -m filename.txt  # Count characters
+    ```
+
+### With standard input
+
+```sh
+echo "Hello World" | ./ccwc -w  # Count words from standard input
+```
+
+## What I learned
